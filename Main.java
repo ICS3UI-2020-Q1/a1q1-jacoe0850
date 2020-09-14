edc7026693b23579d7aaf9a4516b5f5e41e97577
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-14 13:37:59 +0000

    Finished temperature conversion

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+// Ask for the temperature
+System.out.println("Please enter a temperature you would like to convert in degrees Celsius.:");
+// declare and initialize a variable for temperature in Celsius
+    int temp = input.nextInt();
+    // declare and initialize a variable for fahrenheit
+    int fahrenheit = (temp*9)/5+32;
+    // State the temperature in fahrenheit
+    System.out.println(fahrenheit);
   }
 }
